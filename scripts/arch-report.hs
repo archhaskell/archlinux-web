2e0c1ead77a601d2d0e6e9781dc5b31d05631e57
Don Stewart <dons@galois.com>
2010-08-15 22:30:47 +0100

    reports

--- scripts/arch-report.hs
+++ scripts/arch-report.hs
@@ -5,6 +5,7 @@ import Distribution.ArchLinux.Report
 
 --
 -- Assumes the file "arch-haskell-packages" is in the current directory.
+-- Assumes the .build-all.log is in the current directory
 -- To generate this file,
 --
 -- > mypackages | sort > arch-haskell-packages.txt
@@ -13,5 +14,5 @@ import Distribution.ArchLinux.Report
 main = do
     s <- lines `fmap` readFile "arch-haskell-packages.txt"
     writeFile "/tmp/arch-haskell-status.html" =<< report s
-    putStrLn "Now: scp /tmp/arch-haskell-status.html www.galois.com:www/"
+    putStrLn "scp /tmp/arch-haskell-status.html code.haskell.org:/srv/code/arch/"
 
